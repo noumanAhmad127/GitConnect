c932f7c078296f20d6e3797d480134464d2dd9df
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-10-05 02:12:28 +0500

    modified

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
     confirmations: 'users/confirmations'
   }
   resources :profiles, only: %i[index show edit update]
-  # root to: 'profiles#index'
+  root to: 'profiles#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root 'articles#index'
 end
