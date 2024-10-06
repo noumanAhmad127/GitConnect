8f70901571c2a2efc07af6c67e607d51a6fd0ecf
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-10-06 16:57:47 +0500

    Added Projects Controller Functionalities

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,4 +5,43 @@ class ProjectsController < ApplicationController
   def new
     @project = @profile.projects.new
   end
+
+  def create
+    @project = @profile.projects.new(project_params)
+    if @project.save
+      redirect_to profile_path(@profile), noyice: 'Project Created'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @project.create(project_params)
+      redirect_to profile_path(@profile), notice: 'Project Updated'
+    else
+      render :edit, unprocessable_entity
+    end
+  end
+
+  def destroy
+    @project.destroy
+
+    redirect_to profile_path(@profile), notice: 'Project Deleted'
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:profile_id])
+  end
+
+  def set_project
+    @project = @profile.projects.find(params[:id])
+  end
+
+  def project_params
+    params.require(:project).permit(:title, :description, :demo_link, images: [])
+  end
 end
