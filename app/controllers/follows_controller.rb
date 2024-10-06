5e831b1e6111d32158230608e62ea3f5438c6e53
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-10-06 20:00:48 +0500

    Added Follow Controller Functionality

--- app/controllers/follows_controller.rb
+++ app/controllers/follows_controller.rb
@@ -1,2 +1,30 @@
 class FollowsController < ApplicationController
+  before_action :set_profile
+
+  def create
+    @follow = current_user.profile.follower_relationships.build(followed_id: @profile.id)
+
+    if @follow.save
+      redirect_to profile_path(@profile), notice: 'You are now following this developer.'
+    else
+      redirect_to profile_path(@profile), alert: 'Unable to follow this developer.'
+    end
+  end
+
+  def destroy
+    @follow = current_user.profile.followed_relationships.find_id(followed_id: @profile.id)
+
+    if @follow
+      @follow.destroy
+      redirect_to profile_path(@profile), notice: 'You are no longer following this developer.'
+    else
+      redirect_to profile_path(@profile), alert: 'Unable to unfollow this developer.'
+    end
+  end
+
+  private
+
+  def set_profile
+    @profile = Profile.find(params[:profile_id])
+  end
 end
