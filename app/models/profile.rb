d68f523548e54153fa26531097456e6cd5406ee2
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-10-07 17:38:04 +0500

    Modifications

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -16,10 +16,10 @@ class Profile < ApplicationRecord
   end
 
   scope :with_skill_set, lambda { |skill_sets|
-    skill_array = skill_sets.split(',').map(&:strip).map(&:downcase) # Split and downcase skills
+    skill_array = skill_sets.split(',').map(&:strip).map(&:downcase)
     skill_array.map do |skill|
       where('lower(skill_sets::text) LIKE ?', "%#{skill}%")
-    end.reduce(&:or) # Combine conditions using OR
+    end.reduce(&:or) 
   }
   scope :with_location, ->(location) { where('lower(city) = ?', location.downcase) }
   scope :with_years_of_experience, lambda { |years_of_experience|
