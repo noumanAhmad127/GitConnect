f76e175eb06e53f748bc050483db490b81d1b11c
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-10-08 15:03:01 +0500

    Added Post Controller Functionalities

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,56 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: %i[new create edit update destroy]
+  before_action :set_post, only: %i[show edit update destroy]
+  before_action :authorize_user!, only: %i[edit update destroy]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show; end
+
+  def new
+    @post = current_user.profile.posts.new
+  end
+
+  def create
+    @post = current_user.profile.posts.new(post_params)
+
+    if @post.save
+      redirect_to @post, notice: 'Post was successfully created.'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, notice: 'Post was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @post.destroy
+    redirect_to posts_url, notice: 'Post was successfully deleted.'
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content, tag_ids: [])
+  end
+
+  def authorize_user!
+    return if @post.profile == current_user.profile
+
+    redirect_to posts_path, alert: 'You are not authorized to perform this action.'
+  end
 end
