43c95a2cfae668c700a4c218bcfa5393560c17e9
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-10-08 15:45:11 +0500

    updated migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_10_06_144422) do
+ActiveRecord::Schema[7.0].define(version: 2024_10_08_094743) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -52,6 +52,24 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_06_144422) do
     t.index ["follower_id"], name: "index_follows_on_follower_id"
   end
 
+  create_table "post_tags", force: :cascade do |t|
+    t.bigint "post_id", null: false
+    t.bigint "tag_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_tags_on_post_id"
+    t.index ["tag_id"], name: "index_post_tags_on_tag_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "profile_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["profile_id"], name: "index_posts_on_profile_id"
+  end
+
   create_table "profiles", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -81,6 +99,12 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_06_144422) do
     t.index ["profile_id"], name: "index_projects_on_profile_id"
   end
 
+  create_table "tags", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -99,6 +123,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_10_06_144422) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "post_tags", "posts"
+  add_foreign_key "post_tags", "tags"
+  add_foreign_key "posts", "profiles"
   add_foreign_key "profiles", "users"
   add_foreign_key "projects", "profiles"
 end
