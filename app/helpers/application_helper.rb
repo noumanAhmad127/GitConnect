61878191d59d71010580eb71e2665db127112d1d
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-10-09 17:28:17 +0500

    Modifications and styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,6 @@ module ApplicationHelper
     markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
     markdown_to_html.render(text).html_safe
   end
+
+
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,14 @@
 module CommentsHelper
+  def count_comments(comment)
+    total_comments = 1
+    comment.comments.each do |nested_comment|
+      total_comments += count_comments(nested_comment)
+    end
+    total_comments
+  end
+
+  # Method to count all comments for a post (top-level + nested)
+  def total_comment_count(post)
+    post.comments.sum { |comment| count_comments(comment) }
+  end
 end
