2d12a25de575d6698c4f4502837b63a82dced5e8
Nouman Ahmad <ahmadnouman183@gmail.com>
2024-10-10 14:05:46 +0500

    Modifications

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,5 +1,6 @@
 class Follow < ApplicationRecord
-
   belongs_to :follower, class_name: 'Profile'
   belongs_to :followed, class_name: 'Profile'
+
+  validates :follower_id, exclusion: { in: ->(follow) { [follow.followed_id] }, message: "can't follow yourself" }
 end
